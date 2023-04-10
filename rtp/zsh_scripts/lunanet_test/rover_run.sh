c8051c412fc28f2c58ed1fd114b80c3790c6805a
Kyle Vernyi <vernyikyle4@gmail.com>
2023-04-10 17:16:30 -0400

    typo

--- rtp/zsh_scripts/lunanet_test/rover_run.sh
+++ rtp/zsh_scripts/lunanet_test/rover_run.sh
@@ -26,7 +26,7 @@ sleep 6
 cd $HDTN_RTP_DIR
 
 
-./build/bpsendstream --bundle-size=2000 --bundle-rate=0 --use-bp-version-7 \
+./build/bpsend_stream --bundle-size=2000 --bundle-rate=0 --use-bp-version-7 \
         --my-uri-eid=ipn:1.1 --dest-uri-eid=ipn:2.2 --outducts-config-file=$bpsendstream_config \
         --max-incoming-udp-packet-size-bytes=1600 --incoming-rtp-stream-port=$incoming_rtp_port --num-circular-buffer-vectors=500 \
         --enable-rtp-concatenation=false --sdp-filepath="HDTN.sdp" &
