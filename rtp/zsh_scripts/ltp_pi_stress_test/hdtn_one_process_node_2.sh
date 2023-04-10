abc021657b508ba97e68c365181cde29da9a295b
Kyle Vernyi <vernyikyle4@gmail.com>
2023-04-10 14:51:43 -0400

    ltp stress test

---
+++ rtp/zsh_scripts/ltp_pi_stress_test/hdtn_one_process_node_2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# path variables
+config_files=$HDTN_RTP_DIR/config_files/ltp_egress_node_sink_test/
+hdtn_config=$config_files/hdtn_one_process_node_2.json
+
+cd $HDTN_SOURCE_ROOT
+
+# HDTN one process
+./build/module/hdtn_one_process/hdtn-one-process  --contact-plan-file=contactPlanCutThroughMode.json --hdtn-config-file=$hdtn_config &
+one_process_pid=$!
+sleep 10
+
+sleep 360
+
+pkill $one_process_pid
