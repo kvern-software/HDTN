f8c74aee4aaad603544de3266c784db5011b71ec
Kyle Vernyi <vernyikyle4@gmail.com>
2023-04-11 15:16:45 -0400

    port change

--- rtp/zsh_scripts/ltp_pi_stress_test/hdtn_one_process_node_2.sh
+++ rtp/zsh_scripts/ltp_pi_stress_test/hdtn_one_process_node_2.sh
@@ -3,11 +3,12 @@
 # path variables
 config_files=$HDTN_RTP_DIR/config_files/ltp_egress_node_sink_test/
 hdtn_config=$config_files/hdtn_one_process_node_2.json
+contact_plan=$config_files/contact_plan.json
 
 cd $HDTN_SOURCE_ROOT
 
 # HDTN one process
-./build/module/hdtn_one_process/hdtn-one-process  --contact-plan-file=contactPlanCutThroughMode.json --hdtn-config-file=$hdtn_config &
+./build/module/hdtn_one_process/hdtn-one-process  --contact-plan-file=$contact_plan --hdtn-config-file=$hdtn_config &
 one_process_pid=$!
 sleep 10
 
