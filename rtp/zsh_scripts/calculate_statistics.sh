e228df908ae9928773108b22bee06f3ad282fe8f
Kyle Vernyi <vernyikyle4@gmail.com>
2023-04-18 20:07:03 -0400

    test3 udp

--- rtp/zsh_scripts/calculate_statistics.sh
+++ rtp/zsh_scripts/calculate_statistics.sh
@@ -11,7 +11,7 @@
 
 # step one
 test_number=test_2
-test_name=lucia_crf18
+test_name=water_bubble_h264_crf_21
 
 test_media_folder=/home/kyle/nasa/dev/test_media/official_test_media 
 reference_file=$test_media_folder/$test_name.mp4
@@ -20,20 +20,30 @@ distorted_media_folder=/home/kyle/nasa/dev/test_outputs/$test_number/$test_name
 distorted_filename=$test_name
 distorted_file=$distorted_media_folder/$distorted_filename.mp4
 
+
+
 # step two
 reference_length=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $reference_file)
 transmitted_length=$(ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 $distorted_file)
 delta_time=$(echo "$reference_length - $transmitted_length"|bc)
+margin=0.15
+trim_time=$(echo "$delta_time - $margin"|bc)
+window_time=$(echo "$margin + 0.05"|bc)
+
 echo $reference_length
 echo $transmitted_length
 echo $delta_time
-margin=0.2
-window_time=$(echo "$delta_time + $margin"|bc)
+echo $trim_time
 echo $window_time
+# trim reference to be close to distorted to reduce windowing time
+trimmed_reference_name=$distorted_media_folder/trimmed_ref.mp4
+ffmpeg -y  -ss 0$trim_time -i $reference_file -c copy -f mp4 $trimmed_reference_name
 
-# # step three
+
+
+# # # step three
 cd /home/kyle/third_party/easyVmaf
-python3 easyVmaf.py -d  $distorted_file -r $reference_file \
+python3 easyVmaf.py -d  $distorted_file -r $trimmed_reference_name \
 -subsample 10 \
 -sw $window_time \
 -model 4K \
@@ -53,7 +63,7 @@ python3 plot_ssim.py $test_number/$test_name
 python3 plot_psnr.py $test_number/$test_name
 python3 plot_vmaf.py $test_number/$test_name $test_name 
 
-
+echo "Finished computing $test_name statistics."
  
 
 
