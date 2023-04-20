c7d80d7ec797ddf23850fe264164ac04ec023f05
Kyle Vernyi <vernyikyle4@gmail.com>
2023-04-20 13:54:18 -0400

    gstream

--- rtp/zsh_scripts/udp/test_3_point_to_point/gstream/node_2.sh
+++ rtp/zsh_scripts/udp/test_3_point_to_point/gstream/node_2.sh
@@ -7,7 +7,7 @@ output_file_path="/home/$USER/test_outputs/test_3_jetson_to_gateway"
 filename=lucia_crf18_g_15                 # change this for whatever file you want to name
 file=$output_file_path/$filename
 
-mkdir -p  $output_file_path/$filename
+mkdir -p  $output_file_path/$filename/$filename
 
 cd $HDTN_RTP_DIR 
 
