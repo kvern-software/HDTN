83dfdce495aa0955c39391d851ff5c62392ecb35
kyle <vernyikyle4@gmail.com>
2023-07-24 08:26:22 -0400

    critical mutex fix

--- rtp/zsh_scripts/amcrest/send_amcrest.sh
+++ rtp/zsh_scripts/amcrest/send_amcrest.sh
@@ -8,7 +8,10 @@ export GST_DEBUG_DUMP_DOT_DIR=/tmp
 gst-launch-1.0 rtspsrc location=$rtsp_stream_location user-id=admin user-pw=hdtn4nasa \
 ! "application/x-rtp,media=(string)video,payload=(int)96,clockrate=(int)90000" \
 ! queue \
-! udpsink host="127.0.0.1" port=45555 &
+! rtph264depay ! h264parse \
+! avdec_h264 ! videoconvert \
+! videoscale ! video/x-raw,width=3840,height=2160 ! glimagesink &
+# ! udpsink host="127.0.0.1" port=45555 &
 
 #  "application/x-rtp media=video,payload=96,clockrate=90000,encoding-name=H264,packetization-mode=1,profile-level-id=640033,a-packetization-support=DH,a-framerate=30.0"
 sleep 60
